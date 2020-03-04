a8efd6d7df8439776a494824e52f37b7abd18d7d
Jalena Lee <jalenalee@gmail.com>
2020-03-04 14:49:35 -0500

    complete tests i think

--- test/controllers/api/v1/reviews_controller_test.rb
+++ test/controllers/api/v1/reviews_controller_test.rb
@@ -1,12 +1,34 @@
 require 'test_helper'
 
 class Api::V1::ReviewsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
-  test "should get new" do
-    get users_new_url
+  # before test runs, the instance variable review to populate it (using reviews.yml)
+  setup do
+    @review = reviews(:one)
+  end
+  
+  test "should get index" do
+    get api_v1_reviews_path
     assert_response :success
   end
+
+  test "should get show" do 
+    get api_v1_review_path(@review)
+    assert_response :success
+  end 
+
+  test "should get create" do 
+    get api_v1_reviews_path(@review)
+    assert_response :success
+  end 
+
+  test "should get destroy" do 
+    get api_v1_review_path(@review)
+    assert_response :success
+  end 
+
+  test "should get update" do 
+    get api_v1_review_path(@review)
+    assert_response :success
+  end 
+
 end
