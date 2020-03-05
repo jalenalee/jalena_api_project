b9bf60d41f3ba72d13193531f725994f909e5783
Jalena Lee <jalenalee@gmail.com>
2020-03-04 20:39:32 -0500

    complete restaurant controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
       resources :reviews, only: [:index, :show, :create, :destroy, :update]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants, only: [:index, :show, :create, :destroy, :update]
+    end
+  end
 end
